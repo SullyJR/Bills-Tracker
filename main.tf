03cc6079588a912a8a6d6599a5ec1c3c302f085d
SullyJR <sulca807@student.otago.ac.nz>
2024-10-04 00:31:05 +1300

    add manager and tenant ec2

--- main.tf
+++ main.tf
@@ -47,3 +47,57 @@ resource "aws_security_group" "allow_web_and_ssh" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+# EC2 Instances
+resource "aws_instance" "tenant_vm" {
+  ami           = "ami-010e83f579f15bba0"  # Ubuntu AMI, you may need to change this
+  instance_type = "t2.micro"
+  key_name      = "cosc349-2024"  # This is typically the key name in AWS Academy
+
+  vpc_security_group_ids = [aws_security_group.allow_web_and_ssh.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              apt-get update
+              apt-get install -y nodejs npm mysql-client
+              git clone https://github.com/SullyJR/Bills-Tracker
+              cd tenant
+              npm install
+              npm run start
+              EOF
+
+  tags = {
+    Name = "TenantVM"
+  }
+}
+
+resource "aws_instance" "manager_vm" {
+  ami           = "ami-010e83f579f15bba0"  # Ubuntu AMI, you may need to change this
+  instance_type = "t2.micro"
+  key_name      = "cosc349-2024"
+
+  vpc_security_group_ids = [aws_security_group.allow_web_and_ssh.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              apt-get update
+              apt-get install -y nodejs npm mysql-client
+              git clone https://github.com/SullyJR/Bills-Tracker
+              cd manager
+              npm install
+              npm run start
+              EOF
+
+  tags = {
+    Name = "AdminVM"
+  }
+}
+
+# Outputs
+output "tenant_vm_public_ip" {
+  value = aws_instance.tenant_vm.public_ip
+}
+
+output "manager_vm_public_ip" {
+  value = aws_instance.manager_vm.public_ip
+}
