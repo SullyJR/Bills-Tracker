2ddfe164ee494e2294b01efd9385b2cfd83c8302
SullyJR <sulca807@student.otago.ac.nz>
2024-10-07 00:14:27 +1300

    add ports for both apis

--- main.tf
+++ main.tf
@@ -48,6 +48,35 @@ resource "aws_security_group" "allow_web_and_ssh" {
   }
 }
 
+#Open ports for the two APIs
+resource "aws_security_group" "allow_express_backend" {
+  name        = "allow_express_backend"
+  description = "Allow inbound request to both APIs"
+
+  ingress {
+    description = "tenant"
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  ingress {
+    description = "manager"
+    from_port   = 3001
+    to_port     = 3001
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 # EC2 Instances
 resource "aws_instance" "tenant_vm" {
   ami           = "ami-010e83f579f15bba0"  # Ubuntu AMI, you may need to change this
